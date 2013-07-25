acda5e5f7f4a94eeb5c697947cc4bfca1081ae3d
Caleb Johnston <caleb.johnston@controlgroup.com>
2013-07-25 14:31:39 -0400

    Cleaned up XmppListener. Removed XmppInterface in favor of signals.

--- src/XmppListener.cpp
+++ src/XmppListener.cpp
@@ -5,39 +5,31 @@
 using namespace gloox;
 
 XmppListener::XmppListener()
-:	j(NULL), m_session( 0 ), m_messageEventFilter( 0 ), m_chatStateFilter( 0 ), m_isConnected(false), m_server(""), m_username("")
+:	mClient(NULL), mSession( 0 ), mMessageEventFilter( 0 ), mChatStateFilter( 0 ),
+	mIsConnected(false), mServer(""), mUsername("")
 {
-	m_xmppParentHandler = NULL;
-}
-
-XmppListener::XmppListener(XmppInterface* handler)
-:	j(NULL), m_session( 0 ), m_messageEventFilter( 0 ), m_chatStateFilter( 0 ), m_isConnected(false), m_server(""), m_username("")
-{
-	m_xmppParentHandler = handler;
 }
 
 XmppListener::~XmppListener()
 {
-	if (j) j->disconnect();
+	if (mClient) mClient->disconnect();
 	
 //	These lines cause problems??
-//	delete m_session;
-//	delete m_messageEventFilter;
-//	delete m_chatStateFilter;
+//	delete mSession;
+//	delete mMessageEventFilter;
+//	delete mChatStateFilter;
 	
-	delete j;
-	
-	m_xmppParentHandler = NULL;
+	delete mClient;
 }
 
 void XmppListener::setup( const std::string& username, const std::string& server )
 {
-	if ((server.empty() && m_server.empty()) || (username.empty() && m_username.empty())) return;
+	if ((server.empty() && mServer.empty()) || (username.empty() && mUsername.empty())) return;
 	
 	std::string address = username;
 	address += "@";
 	if (server.empty()){
-		address += m_server;
+		address += mServer;
 	} 
 	else {
 		address += server;
@@ -45,26 +37,25 @@ void XmppListener::setup( const std::string& username, const std::string& server
 	
 	JID jid(address);
 	
-	j = new Client( jid, username );
-	j->registerPresenceHandler( this );
-	j->registerConnectionListener( this );
-	j->registerMessageSessionHandler( this, 0 );
-	gloox::RosterListener* rl = this->getRosterListenerCast();
-	j->rosterManager()->registerRosterListener(rl);
-	j->disco()->setVersion( "messageTest", GLOOX_VERSION, "Linux" );
-	j->disco()->setIdentity( "client", "bot" );
-	j->disco()->addFeature( XMLNS_CHAT_STATES );
+	mClient = new Client( jid, username );
+	mClient->registerPresenceHandler( this );
+	mClient->registerConnectionListener( this );
+	mClient->registerMessageSessionHandler( this, 0 );
+	mClient->rosterManager()->registerRosterListener(this);
+	mClient->disco()->setVersion( "messageTest", GLOOX_VERSION, "Linux" );
+	mClient->disco()->setIdentity( "client", "bot" );
+	mClient->disco()->addFeature( XMLNS_CHAT_STATES );
 
 	//j->logInstance().registerLogHandler( LogLevelDebug, LogAreaAll, this );
 }
 
 void XmppListener::openConnection(bool toggle)
 {
-	if (!m_isConnected && j) {
-		if ( j->connect( toggle ) ) {
+	if (!mIsConnected && mClient) {
+		if ( mClient->connect( toggle ) ) {
 			ConnectionError ce = ConnNoError;
-			while ( ce == ConnNoError && j ) {
-				ce = j->recv();
+			while ( ce == ConnNoError && mClient ) {
+				ce = mClient->recv();
 			}
 		}
 	}
@@ -72,27 +63,24 @@ void XmppListener::openConnection(bool toggle)
 
 void XmppListener::onConnect()
 {
-	m_xmppParentHandler->onConnect();
+	mSignalConnect();
 	
-	m_isConnected = true;
+	mIsConnected = true;
 }
 
 void XmppListener::onDisconnect( ConnectionError e )
 {
 	if ( e == ConnAuthenticationFailed ) {
-		ci::app::console() << "Auth failed. reason: " << j->authError() << std::endl;
+		ci::app::console() << "Auth failed. reason: " << mClient->authError() << std::endl;
 	}
 	
-	m_xmppParentHandler->onDisconnect(e);
+	mIsConnected = false;
 	
-	m_isConnected = false;
+	mSignalDisconnect();
 }
 
 bool XmppListener::onTLSConnect( const CertInfo& info )
 {
-	time_t from( info.date_from );
-	time_t to( info.date_to );
-	
 	// info.status
 	// info.issuer
 	// info.server
@@ -100,73 +88,75 @@ bool XmppListener::onTLSConnect( const CertInfo& info )
 	// info.mac
 	// info.cipher
 	// info.compression
+	// time_t from( info.date_from );
+	// time_t to( info.date_to );
 	// ctime( &from )
 	// ctime( &to )
 		
-	m_xmppParentHandler->onTLSConnect(info);
+	mSignalTlsConnect(info);
 		
 	return true;
 }
 
-void XmppListener::handleMessage( const Message& msg, MessageSession * sess )
+void XmppListener::handleMessage( const Message& msg, MessageSession* sess )
 {
 	// msg.subtype()
 	// msg.subject()
 	// msg.body()
 	// msg.thread()
 
-	m_xmppParentHandler->handleMessage(msg, sess);
+	mSignalHandleMsg(msg, sess);
 }
 
 void XmppListener::handleMessageEvent( const JID& from, MessageEventType event ) {}
 
 void XmppListener::handleChatState( const JID& from, ChatStateType state ) {}
 
-void XmppListener::handleMessageSession( MessageSession *session )
+void XmppListener::handleMessageSession( MessageSession* session )
 {
 	// this example can handle only one session. so we get rid of the old session
-	j->disposeMessageSession( m_session );
-	m_session = session;
-	gloox::MessageHandler* mh = this->getMessageHandlerCast();
-	m_session->registerMessageHandler( mh );
-	m_messageEventFilter = new MessageEventFilter( m_session );
-	m_messageEventFilter->registerMessageEventHandler( this );
-	m_chatStateFilter = new ChatStateFilter( m_session );
-	m_chatStateFilter->registerChatStateHandler( this );
+	mClient->disposeMessageSession( mSession );
+	mSession = session;
+	mSession->registerMessageHandler( this );
+	mMessageEventFilter = new MessageEventFilter( mSession );
+	mMessageEventFilter->registerMessageEventHandler( this );
+	mChatStateFilter = new ChatStateFilter( mSession );
+	mChatStateFilter->registerChatStateHandler( this );
 }
 
 void XmppListener::handleRoster( const Roster& roster )
 {	
-	m_xmppParentHandler->handleRoster(roster);
+	mSignalRoster(roster);
 }
 
-void XmppListener::handleRosterPresence( const RosterItem& item, const std::string& resource, Presence::PresenceType presence, const std::string& msg )
+void XmppListener::handleRosterPresence(const RosterItem& item, const std::string& resource,
+										Presence::PresenceType presence, const std::string& msg )
 {
 	//item.jid()
 	//presence
 	
-	m_xmppParentHandler->handleRosterPresence(item, resource, presence, msg);
+	mSignalRosterPresence(item, resource, presence, msg);
 }
 
 void XmppListener::handlePresence( const Presence& presence ) {}
 
 void XmppListener::handleLog( LogLevel level, LogArea area, const std::string& message )
 {
-	ci::app::console() << "XMPP Log Level: " << level << ", area: " << area << " : " << message.c_str() << std::endl;
+	//ci::app::console() << "XMPP Log Level: " << level << ", area: " << area << " : " << message.c_str() << std::endl;
 }
 
 bool XmppListener::sendMessage( const JID& recipient, const std::string& msg, const std::string& subject )
 {
-	if(!m_isConnected || msg.empty() || j == NULL) return false;
+	if(!mIsConnected || msg.empty() || mClient == NULL) return false;
 	
 	Message m(Message::Chat, recipient, msg, subject);
 	
-	j->send(m);
+	mClient->send(m);
 	
 	return true;
 }
 
 gloox::Client& XmppListener::getClient() const
 {
-	return *j;
+	return *mClient;
 }
--- src/XmppListener.h
+++ src/XmppListener.h
@@ -1,9 +1,13 @@
 #pragma once
 
-#include "XmppInterface.h"
-
 #include "gloox/gloox.h"
 #include "gloox/client.h"
+#include "gloox/message.h"
+#include "gloox/rostermanager.h"
+#include "gloox/messagehandler.h"
+#include "gloox/messagesession.h"
+#include "gloox/rosterlistener.h"
+#include "gloox/connectionlistener.h"
 #include "gloox/presencehandler.h"
 #include "gloox/messagesessionhandler.h"
 #include "gloox/messageeventhandler.h"
@@ -29,17 +33,37 @@
 # include <windows.h>
 #endif
 
-class XmppListener : public cg::esi::XmppInterface, gloox::MessageSessionHandler, gloox::LogHandler,
-	gloox::MessageEventHandler, gloox::ChatStateHandler, gloox::PresenceHandler {
+typedef ci::signals::signal<void()> SignalCallback;
+typedef ci::signals::signal<void(const gloox::CertInfo&)> SignalCallbackTls;
+typedef ci::signals::signal<void(const gloox::Roster&)> SignalCallbackRoster;
+typedef ci::signals::signal<void(const gloox::Message&, gloox::MessageSession*)> SignalCallbackMsg;
+typedef ci::signals::signal<void(const gloox::RosterItem&, const std::string&,
+									gloox::Presence::PresenceType, const std::string&)> SignalCallbackRosterPresence;
+
+class XmppListener : public gloox::ConnectionListener, gloox::MessageHandler,
+	gloox::RosterListener, gloox::MessageSessionHandler, gloox::LogHandler,
+	gloox::MessageEventHandler, gloox::ChatStateHandler, gloox::PresenceHandler
+{
 public:
 	XmppListener();
-	XmppListener(XmppInterface* handler);
 	virtual ~XmppListener();
+
 	void setup( const std::string& username = "", const std::string& server = "" );
+
 	void openConnection(bool toggle = false);
+
 	gloox::Client& getClient() const;
+		
+	bool isConnected() const { return mIsConnected; }
 	
 	virtual bool sendMessage( const gloox::JID& recipient, const std::string& msg, const std::string& subject );
+	
+	SignalCallback&					getSignalConnect() { return mSignalConnect; }
+	SignalCallbackTls&				getSignalTlsConnect() { return mSignalTlsConnect; }
+	SignalCallback&					getSignalDisconnect() { return mSignalDisconnect; }
+	SignalCallbackMsg&				getSignalMessage() { return mSignalHandleMsg; }
+	SignalCallbackRoster&			getSignalRoster() { return mSignalRoster; }
+	SignalCallbackRosterPresence&	getSignalRosterPresence() { return mSignalRosterPresence; }
 	
 protected:
 	virtual void onConnect();
@@ -54,7 +78,8 @@ protected:
 
 	// satisfy interfaces for gloox::RosterListener
 	virtual void handleRoster( const gloox::Roster& roster );
-	virtual void handleRosterPresence( const gloox::RosterItem& item, const std::string& resource, gloox::Presence::PresenceType presence, const std::string& msg );
+	virtual void handleRosterPresence( const gloox::RosterItem& item, const std::string& resource,
+									  gloox::Presence::PresenceType presence, const std::string& msg );
 	virtual void handleRosterError( const gloox::IQ& iq ) {}
 	virtual void handleItemAdded( const gloox::JID& jid ) {}
 	virtual void handleItemSubscribed( const gloox::JID& jid ) {}
@@ -68,13 +93,17 @@ protected:
 	virtual void handleNonrosterPresence( const gloox::Presence& presence ) {}
 
 private:	
-	bool m_isConnected;
+	bool mIsConnected;
 	
-	gloox::Client *j;
-	gloox::MessageSession *m_session;
-	gloox::MessageEventFilter *m_messageEventFilter;
-	gloox::ChatStateFilter *m_chatStateFilter;
-	std::string m_server;
-	cg::esi::XmppInterface* m_xmppParentHandler;
-	std::string m_username;
+	gloox::Client* mClient;
+	gloox::MessageSession* mSession;
+	gloox::MessageEventFilter* mMessageEventFilter;
+	gloox::ChatStateFilter* mChatStateFilter;
+	std::string mServer;
+	std::string mUsername;
+	SignalCallback mSignalConnect, mSignalDisconnect;
+	SignalCallbackTls mSignalTlsConnect;
+	SignalCallbackMsg mSignalHandleMsg;
+	SignalCallbackRoster mSignalRoster;
+	SignalCallbackRosterPresence mSignalRosterPresence;
 };
